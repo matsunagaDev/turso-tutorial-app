ae8178a9e68fbf0e34af367409e1e2fb0f2091a6
masarumatsunaga <65140122+masarumatsunaga@users.noreply.github.com>
2026-01-04 18:42:40 +0900

    drizzle connected

--- db/index.ts
+++ db/index.ts
@@ -2,8 +2,8 @@ import { drizzle } from 'drizzle-orm/libsql/web';
 
 export const db = drizzle({
   connection: {
-    url: process.env.DATABASE_URL,
-    authToken: process.env.DATABASE_AUTH_TOKEN,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
   schema: {
     // ...authSchema,
